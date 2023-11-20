3f8506538f4978cc08c171d9eec9e56df611a0ff
billy210 <wschap@ufl.edu>
2023-11-20 16:52:43 -0500

    Update inject-recover-MP.py
    injection was previously operating on a non-normalized light curve, and thus the resulting injected signal was significantly smaller than expected from a planet of a given size. Injection now results in signals that match the expected transit depth based on (Rp/R*)**2

--- inject-recover-MP.py
+++ inject-recover-MP.py
@@ -1,4 +1,55 @@
-def injectplanetinjecthardcut(arg):
+from IPython.display import Image
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import eleanor
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from astropy.modeling.models import BlackBody
+from astropy import units as u
+from astropy.modeling import models
+from astropy.coordinates import SkyCoord
+import astropy.coordinates as coord
+from astropy.io import ascii
+from astropy import table
+from astropy.io.votable import parse
+from lightkurve.correctors import PLDCorrector
+from astropy.io import fits
+import lightkurve as lk
+from astroquery.gaia import Gaia
+Gaia.MAIN_GAIA_TABLE = "gaiadr2.gaia_source"  # Select Data Release 2
+Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"  # Reselect Data Release 3, default
+import time
+import multiprocessing as mp
+import batman
+from astropy.timeseries import BoxLeastSquares
+from pylab import *
+from IPython.display import Image,display
+import math
+from astroquery.mast import Observations
+from astropy.convolution import convolve, Box1DKernel
+import os
+from tglc.quick_lc import tglc_lc
+import glob
+
+from astropy.utils.exceptions import AstropyWarning
+warnings.simplefilter('ignore', category=AstropyWarning)
+
+
+plt.rcParams['figure.dpi'] = 120
+
+binsizedays = 30/60/24
+
+##############################################
+# New and updated version for testing
+# Will hopefully match the outputs from the test script above after integration of new analysis
+#############################################
+
+
+def injectplanetbeforestitch(arg):
     idx,row = arg
     #normalized_corrected = lcbin
     depth = row['depths']
@@ -18,85 +69,106 @@ def injectplanetinjecthardcut(arg):
     params.u = [0.1, 0.3]                #limb darkening coefficients [u1, u2]
     params.limb_dark = "quadratic"       #limb darkening model
 
-    # Define the times at which to evaluate the fake transit
-    t=binned_lc.time.value
-
-    # Create the batman transit model
-    m = batman.TransitModel(params, t)
-
-    # Generate the fake light curve transit
-    injected_model = m.light_curve(params)
-
-
-    # Inject the fake transit into the real data
-    injected_flux = binned_lc.flux.value + injected_model - 1.0
-
-
-    lc_injected=binned_lc.copy()
-    lc_injected.flux = injected_flux
-    lc_injected= lc_injected.copy() #we need this, it makes sure the flux u.quantity works with my math, otherwise if you do .remove_outliers in next step its fine
+    
     ###################################
     ##clean after injecting and proceed
     ###################################
-    lc_injected = lc_injected.remove_outliers(sigma=5).remove_nans()
-    endtime = max(lc_injected.time.value)-1
-    starttime = min(lc_injected.time.value)+1
-    lc_injected = lc_injected[(lc_injected['time'].value>starttime) & (lc_injected['time'].value<endtime)]
-    lc_injected = lc_injected.remove_nans()
+    data = []
+    for s in inject_lc: 
+        datum =  eleanor.TargetData(s, height=15, width=15, bkg_size=31, do_psf=False, do_pca=True, regressors='corner')
+        q = datum.quality == 0
+        
+        lc_orig = datum.to_lightkurve()
+        lc_orig = lc_orig.normalize() ##MAKE SURE TO NORMALIZE BEFORE INJECTING else you add tiny number to raw flux val
+            
+        # Define the times at which to evaluate the fake transit
+        t=lc_orig.time.value
 
-    gapsize = (lc_injected.time.value[1::] - lc_injected.time.value[:-1:])
-    dt = np.median(gapsize)
-    lc_injected['gapsize']=np.append(gapsize,0)
+        # Create the batman transit model
+        m = batman.TransitModel(params, t)
 
-    lc_injected['index'] = np.arange(len(lc_injected))
-    #cleanlc.add_index('index')
-    
-    for row in lc_injected:
-        if row['gapsize']>0.5:
-            idmax = row['index']+50 #removes 3.3 hrs of 200s cadence
-            idmin = row['index']-49
-            lc_injected['flux'][idmin:idmax]=np.nan
-    lc_injected = lc_injected.remove_nans()
-    lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
-
-    ###############################
-    # cut things that spike upwards
-    ###############################
-
-    sigma_upper = 4
-    std = np.std(lc_injected['flux'])
-    #print(std)
-    for row in lc_injected:
-        if row['flux']>(1 + (sigma_upper * std)):
-            idmax = row['index']+20
-            idmin = row['index']-19
-            lc_injected['flux'][idmin:idmax]=np.nan 
-    lc_injected = lc_injected.remove_nans()
-    lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
+        # Generate the fake light curve transit
+        injected_model = m.light_curve(params)
 
 
+        # Inject the fake transit into the real data
+        injected_flux = lc_orig.flux.value + injected_model - 1.0
 
-    lc_injected = lc_injected[(lc_injected['flux']<1.20)&(lc_injected['flux']>0.80)].remove_nans().bin(time_bin_size=binsizedays)
-    lc_injected['index'] = np.arange(len(lc_injected))#reindex to avoid issues with removing points
-    #################################################
-    # cut below expected signal of a 3.5 R_jup planet
-    #################################################
-    #lower_lim = 0.5
-    lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
-    for row in lc_injected:
-        if row['flux']<lower_lim:
-            idmax = row['index']+2
-            idmin = row['index']-1
-            lc_injected['flux'][idmin:idmax]=np.nan
 
-    lc_injected = lc_injected.remove_nans()
+        lc_injected=lc_orig.copy()
+        lc_injected.flux = injected_flux
+        lc_injected= lc_injected.copy() #we need this, it makes sure the flux u.quantity works with my math, otherwise if you do .remove_outliers in next step its fine
+
+        
+        lc_injected = lc_injected.remove_outliers(sigma=6).normalize().remove_nans()
+        endtime = max(lc_injected.time.value)-1
+        starttime = min(lc_injected.time.value)+1
+        lc_injected = lc_injected[(lc_injected['time'].value>starttime) & (lc_injected['time'].value<endtime)]
+        lc_injected = lc_injected.remove_nans()
+
+        gapsize = (lc_injected.time.value[1::] - lc_injected.time.value[:-1:])
+        dt = np.median(gapsize)
+        lc_injected['gapsize']=np.append(gapsize,0)
+
+        lc_injected['index'] = np.arange(len(lc_injected))
+        #cleanlc.add_index('index')
+
+        for row in lc_injected:
+            if row['gapsize']>0.5:
+                idmax = row['index']+50 #removes 3.3 hrs of 200sec cadence
+                idmin = row['index']-49
+                lc_injected['flux'][idmin:idmax]=np.nan
+        lc_injected = lc_injected.remove_nans()
+        lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
+
+        ###############################
+        # cut things that spike upwards
+        ###############################
+        sigma_upper = 4
+        std = np.std(lc_injected['flux'])
+        #print(std)
+        for row in lc_injected:
+            if row['flux']>(1 + (sigma_upper * std)):
+                #print(row['flux'])
+                idmax = row['index']+20
+                idmin = row['index']-19
+                lc_injected['flux'][idmin:idmax]=np.nan    
+        lc_injected = lc_injected.remove_nans()
+        lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
+
+
+
+        lc_injected = lc_injected[(lc_injected['flux']<1.20)&(lc_injected['flux']>0.80)].remove_nans()#.bin(time_bin_size=binsizedays)
+        #noinject = noinject.remove_nans().bin(time_bin_size=binsizedays)
+        lc_injected['index'] = np.arange(len(lc_injected))#reindex to avoid issues with removing points
+
+        #################################################
+        # cut below expected signal of a 3.5 R_jup planet
+        #################################################
+        #lower_lim = 0.5
+
+        lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        for row in lc_injected:
+            if row['flux']<lower_lim:
+                idmax = row['index']+2
+                idmin = row['index']-1
+                lc_injected['flux'][idmin:idmax]=np.nan
+
+
+        lc_injected = lc_injected.bin(time_bin_size=binsizedays).remove_nans()
+        data.append(lc_injected)
+        
+
+    collect = lk.LightCurveCollection(data)
+
+    lc_injected = collect.stitch()
     
     #fig,axs=plt.subplots(3,1,figsize=(10,10))
     planetrad = depth*stellar_radius * 9.73116 #convert the solar radii to jupiter radii
     #lc_injected.scatter(ax=axs[0],s=25,color='r',label='injected transit signal')
     #normalized_corrected.scatter(ax=axs[0],s=25)
     
-    period_grid = np.linspace(0.6, 11, 10000)
+    period_grid = np.linspace(0.6, 11, 25000)
     bls = lc_injected.to_periodogram(method='bls', period=period_grid, frequency_factor=700);
     #bls.plot(ax=axs[1],label=f'best p = {bls.period_at_max_power:.2f}');
     planet_b_period = bls.period_at_max_power
@@ -146,81 +218,104 @@ def injectplanetinjecthardcut(arg):
 
 
 
-def vary_paramsmult(func, input_lc, df,stellar_radius=1.325, trials=10000, num_processes=1):
+def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_processes=1):
     t0 = time.time()
     
-    binned_lc = input_lc.copy()
-    noinject = input_lc.copy()
-    #noinject.scatter()
-    print(np.mean(binned_lc['flux']))
-    print(np.mean(noinject['flux']))
-
-    noinject = noinject.remove_outliers(sigma=5).remove_nans()
-    endtime = max(noinject.time.value)-1
-    starttime = min(noinject.time.value)+1
-    noinject = noinject[(noinject['time'].value>starttime) & (noinject['time'].value<endtime)]
-    noinject = noinject.remove_nans()
-
-    gapsize = (noinject.time.value[1::] - noinject.time.value[:-1:])
-    dt = np.median(gapsize)
-    noinject['gapsize']=np.append(gapsize,0)
-
-    noinject['index'] = np.arange(len(noinject))
-    #cleanlc.add_index('index')
+    noinjectstar = star.copy()
+    #injectstar = star.copy()
     
-    for row in noinject:
-        if row['gapsize']>0.5:
-            idmax = row['index']+50 #removes 3.3 hrs of 200s cadence
-            idmin = row['index']-49
-            noinject['flux'][idmin:idmax]=np.nan
-    noinject = noinject.remove_nans()
-    noinject['index'] = np.arange(len(noinject)) #reindex to avoid issues with removing points
+    data = []
+    for s in noinjectstar:   
+        datum =  eleanor.TargetData(s, height=15, width=15, bkg_size=31, aperture_mode='small', do_psf=False, do_pca=True, regressors='corner')
+        q = datum.quality == 0
 
-    ###############################
-    # cut things that spike upwards
-    ###############################
+        lc_orig = datum.to_lightkurve()
+        lc_orig = lc_orig.normalize() ##MAKE SURE TO NORMALIZE BEFORE INJECTING else you add tiny number to raw flux val
 
-    sigma_upper = 4
-    std = np.std(noinject['flux'])
-    #print(std)
-    for row in noinject:
-        if row['flux']>(1 + (sigma_upper * std)):
-            idmax = row['index']+20
-            idmin = row['index']-19
-            noinject['flux'][idmin:idmax]=np.nan    
-    noinject = noinject.remove_nans()
-    noinject['index'] = np.arange(len(noinject)) #reindex to avoid issues with removing points
+        lc_orig= lc_orig.copy() #we need this, it makes sure the flux u.quantity works with my math, otherwise if you do .remove_outliers in next step its fine
+
+        #lc_injected.scatter(c='red')
+        #lc_orig.scatter()
+        ###xtime = lc_injected['time'].copy()
+        ###yflux = lc_injected['flux'].copy()
+        ###plt.scatter(xtime,yflux,c='r')
+        ###plt.scatter(lc_orig['time'],lc_orig['flux'])
+        ###plt.show()
+
+        lc_orig = lc_orig.remove_outliers(sigma=6).normalize().remove_nans()
+
+        endtime = max(lc_orig.time.value)-1
+        starttime = min(lc_orig.time.value)+1
+        lc_orig = lc_orig[(lc_orig['time'].value>starttime) & (lc_orig['time'].value<endtime)]
+        lc_orig = lc_orig.remove_nans()
+
+        gapsize = (lc_orig.time.value[1::] - lc_orig.time.value[:-1:])
+        dt = np.median(gapsize)
+        lc_orig['gapsize']=np.append(gapsize,0)
+
+        lc_orig['index'] = np.arange(len(lc_orig))
+        #cleanlc.add_index('index')
+
+        for row in lc_orig:
+            if row['gapsize']>0.5:
+                idmax = row['index']+50 #removes 3.3 hrs of 200sec cadence
+                idmin = row['index']-49
+                lc_orig['flux'][idmin:idmax]=np.nan
+        lc_orig = lc_orig.remove_nans()
+        lc_orig['index'] = np.arange(len(lc_orig)) #reindex to avoid issues with removing points
+
+        ###############################
+        # cut things that spike upwards
+        ###############################
+        sigma_upper = 4
+        std = np.std(lc_orig['flux'])
+        #print(std)
+        for row in lc_orig:
+            if row['flux']>(1 + (sigma_upper * std)):
+                #print(row['flux'])
+                idmax = row['index']+20
+                idmin = row['index']-19
+                lc_orig['flux'][idmin:idmax]=np.nan    
+        lc_orig = lc_orig.remove_nans()
+        lc_orig['index'] = np.arange(len(lc_orig)) #reindex to avoid issues with removing points
 
 
 
-    noinject = noinject[(noinject['flux']<1.20)&(noinject['flux']>0.80)].remove_nans().bin(time_bin_size=binsizedays)
-    #noinject = noinject.remove_nans().bin(time_bin_size=binsizedays)
-    noinject['index'] = np.arange(len(noinject))#reindex to avoid issues with removing points
-    #################################################
-    # cut below expected signal of a 3.5 R_jup planet
-    #################################################
-    #lower_lim = 0.5
-    lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
-    for row in noinject:
-        if row['flux']<lower_lim:
-            idmax = row['index']+2
-            idmin = row['index']-1
-            noinject['flux'][idmin:idmax]=np.nan
+        lc_orig = lc_orig[(lc_orig['flux']<1.20)&(lc_orig['flux']>0.80)].remove_nans()#.bin(time_bin_size=binsizedays)
+        lc_orig['index'] = np.arange(len(lc_orig))#reindex to avoid issues with removing points
 
-    noinject = noinject.remove_nans()
+        #################################################
+        # cut below expected signal of a 3.5 R_jup planet
+        #################################################
+        #lower_lim = 0.5
+
+        lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        for row in lc_orig:
+            if row['flux']<lower_lim:
+                idmax = row['index']+2
+                idmin = row['index']-1
+                lc_orig['flux'][idmin:idmax]=np.nan
+
+
+        lc_orig = lc_orig.bin(time_bin_size=binsizedays).remove_nans()
+        lc_orig = lc_orig.remove_nans()
+
+        data.append(lc_orig)
+
+
+
+    collect = lk.LightCurveCollection(data)
+
+    noinject = collect.stitch()
+    noinject.scatter()
     
     def init():
-        global binned_lc
-        binned_lc = input_lc.copy()
-        global noinject_lc
+        global inject_lc #lc to have planet injected
+        inject_lc = noinjectstar
+        global noinject_lc #reduced lightcurve of star without injection
         noinject_lc = noinject.copy()
     
 
-        #global binned_lc
-        #binned_lc = input_lc.copy()
-        #global noinject
-        #noinject = noinject.copy()
-    #with mp.Pool(num_processes,initializer=init, initargs=(input_lc,),maxtasksperchild=10) as pool:
     with mp.Pool(num_processes,initializer=init,maxtasksperchild=10) as pool:
         ##binned_lc.scatter()
         ##noinject.scatter()
@@ -230,7 +325,7 @@ def vary_paramsmult(func, input_lc, df,stellar_radius=1.325, trials=10000, num_p
         rad_max = 0.2/stellar_radius #now in R_hoststar for a 2 Jupiter radii planet
 
         depths = np.random.uniform(rad_min, rad_max, trials)  # random transit depths to inject
-        midtimes = np.random.uniform(min(binned_lc.time.value), max(binned_lc.time.value), trials)  # mid-transit times to inject if you want
+        midtimes = np.random.uniform(min(noinject.time.value), max(noinject.time.value), trials)  # mid-transit times to inject if you want
         periods = np.random.uniform(0.6,11,trials) # periods to inject
         
         df['depths'] = depths
@@ -256,5 +351,15 @@ def vary_paramsmult(func, input_lc, df,stellar_radius=1.325, trials=10000, num_p
     print(f'Finished in {total}s')
     return df
 
-#output_table_currents3 = pd.DataFrame()
-#output_table_currents3 = vary_paramsmult(injectplanetinjecthardcut,s3lcbin, output_table_currents3, stellar_radius=0.681, trials=1, num_processes=128)
+
+
+#stellar_radius = 0.943
+#tic = 77463924
+#Tmag = 16.931
+#star = eleanor.multi_sectors(sectors='all',tic=tic)
+#input_lc_file = star
+#n_trials = 1000
+#n_processes = 96
+#output_table_lcbin = pd.DataFrame()
+#output_table_lcbin77463924 = vary_paramsmult(injectplanetbeforestitch,input_lc_file, output_table_lcbin, stellar_radius=stellar_radius, trials=n_trials, num_processes=n_processes)
+
