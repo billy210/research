1c4d17fdf96da7b06788e54621ce793982eeb5b0
billy210 <wschap@ufl.edu>
2023-12-06 17:13:17 -0500

    Update inject-recover-MP.py
    Fixed issue where too many normalizations were occurring

--- inject-recover-MP.py
+++ inject-recover-MP.py
@@ -74,13 +74,13 @@ def injectplanetbeforestitch(arg):
     ##clean after injecting and proceed
     ###################################
     data = []
-    for s in inject_lc: 
-        datum =  eleanor.TargetData(s, height=15, width=15, bkg_size=31, do_psf=False, do_pca=True, regressors='corner')
+    for s in inject_lc:    
+        datum =  eleanor.TargetData(s, height=15, width=15, bkg_size=31, aperture_mode='small', do_psf=False, do_pca=True, regressors='corner')
         q = datum.quality == 0
-        
+
         lc_orig = datum.to_lightkurve()
-        lc_orig = lc_orig.normalize() ##MAKE SURE TO NORMALIZE BEFORE INJECTING else you add tiny number to raw flux val
-            
+        lc_orig = lc_orig.copy().normalize() ##MAKE SURE TO NORMALIZE BEFORE INJECTING else you add tiny number to raw flux val
+
         # Define the times at which to evaluate the fake transit
         t=lc_orig.time.value
 
@@ -90,22 +90,39 @@ def injectplanetbeforestitch(arg):
         # Generate the fake light curve transit
         injected_model = m.light_curve(params)
 
+        #plt.plot(t, injected_model)
+        #plt.xlabel("Time from central transit")
+        #plt.ylabel("Relative flux")
+        #plt.show()
+
 
         # Inject the fake transit into the real data
         injected_flux = lc_orig.flux.value + injected_model - 1.0
-
+        #injected_flux = lc_orig.flux.value * injected_model
 
         lc_injected=lc_orig.copy()
         lc_injected.flux = injected_flux
         lc_injected= lc_injected.copy() #we need this, it makes sure the flux u.quantity works with my math, otherwise if you do .remove_outliers in next step its fine
 
-        
-        lc_injected = lc_injected.remove_outliers(sigma=6).normalize().remove_nans()
+
+
+        lc_injected = lc_injected.remove_outliers(sigma=6).remove_nans()#.normalize().remove_nans()
+        ##lc_orig = lc_orig.remove_outliers(sigma=6).remove_nans()#.normalize().remove_nans()
+
+
         endtime = max(lc_injected.time.value)-1
         starttime = min(lc_injected.time.value)+1
         lc_injected = lc_injected[(lc_injected['time'].value>starttime) & (lc_injected['time'].value<endtime)]
         lc_injected = lc_injected.remove_nans()
 
+        ##endtime = max(lc_orig.time.value)-1
+        ##starttime = min(lc_orig.time.value)+1
+        ##lc_orig = lc_orig[(lc_orig['time'].value>starttime) & (lc_orig['time'].value<endtime)]
+        ##lc_orig = lc_orig.remove_nans()
+
+
+
+        ############################################################################
         gapsize = (lc_injected.time.value[1::] - lc_injected.time.value[:-1:])
         dt = np.median(gapsize)
         lc_injected['gapsize']=np.append(gapsize,0)
@@ -120,6 +137,27 @@ def injectplanetbeforestitch(arg):
                 lc_injected['flux'][idmin:idmax]=np.nan
         lc_injected = lc_injected.remove_nans()
         lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
+
+
+        ############################################################################
+        '''
+        #only for diagnostic prints
+        gapsize = (lc_orig.time.value[1::] - lc_orig.time.value[:-1:])
+        dt = np.median(gapsize)
+        lc_orig['gapsize']=np.append(gapsize,0)
+
+        lc_orig['index'] = np.arange(len(lc_orig))
+        #cleanlc.add_index('index')
+
+        for row in lc_orig:
+            if row['gapsize']>0.5:
+                idmax = row['index']+50 #removes 3.3 hrs of 200sec cadence
+                idmin = row['index']-49
+                lc_orig['flux'][idmin:idmax]=np.nan
+        lc_orig = lc_orig.remove_nans()
+        lc_orig['index'] = np.arange(len(lc_orig)) #reindex to avoid issues with removing points
+        '''
+
 
         ###############################
         # cut things that spike upwards
@@ -137,38 +175,63 @@ def injectplanetbeforestitch(arg):
         lc_injected['index'] = np.arange(len(lc_injected)) #reindex to avoid issues with removing points
 
 
+        '''
+        ###############################
+        # cut things that spike upwards
+        ###############################
+        sigma_upper = 4
+        std = np.std(lc_orig['flux'])
+        #print(std)
+        for row in lc_orig:
+            if row['flux']>(1 + (sigma_upper * std)):
+                #print(row['flux'])
+                idmax = row['index']+20
+                idmin = row['index']-19
+                lc_orig['flux'][idmin:idmax]=np.nan    
+        lc_orig = lc_orig.remove_nans()
+        lc_orig['index'] = np.arange(len(lc_orig)) #reindex to avoid issues with removing points
+        '''
 
-        lc_injected = lc_injected[(lc_injected['flux']<1.20)&(lc_injected['flux']>0.80)].remove_nans()#.bin(time_bin_size=binsizedays)
+
+
+        lc_injected = lc_injected[(lc_injected['flux']<1.20)&(lc_injected['flux']>0.70)].remove_nans()#.bin(time_bin_size=binsizedays)
         #noinject = noinject.remove_nans().bin(time_bin_size=binsizedays)
         lc_injected['index'] = np.arange(len(lc_injected))#reindex to avoid issues with removing points
 
+        '''
         #################################################
         # cut below expected signal of a 3.5 R_jup planet
         #################################################
         #lower_lim = 0.5
 
-        lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        #lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        lower_lim = (1-(((3/9.73116)/stellar_radius)**2))
         for row in lc_injected:
             if row['flux']<lower_lim:
                 idmax = row['index']+2
                 idmin = row['index']-1
                 lc_injected['flux'][idmin:idmax]=np.nan
 
-
+        '''
         lc_injected = lc_injected.bin(time_bin_size=binsizedays).remove_nans()
+        ##lc_orig = lc_orig.bin(time_bin_size=binsizedays).remove_nans()
+
         data.append(lc_injected)
-        
+        ##odata.append(lc_orig)
+
 
     collect = lk.LightCurveCollection(data)
+    ##ocollect = lk.LightCurveCollection(odata)
 
-    lc_injected = collect.stitch()
+    lc_injected = collect.stitch(lambda x: x.remove_nans())
+
     
     #fig,axs=plt.subplots(3,1,figsize=(10,10))
     planetrad = depth*stellar_radius * 9.73116 #convert the solar radii to jupiter radii
     #lc_injected.scatter(ax=axs[0],s=25,color='r',label='injected transit signal')
     #normalized_corrected.scatter(ax=axs[0],s=25)
     
-    period_grid = np.linspace(0.6, 11, 25000)
+    period_grid = np.linspace(0.6, 10, 25000)
     bls = lc_injected.to_periodogram(method='bls', period=period_grid, frequency_factor=700);
     #bls.plot(ax=axs[1],label=f'best p = {bls.period_at_max_power:.2f}');
     planet_b_period = bls.period_at_max_power
@@ -225,15 +288,15 @@ def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_proce
     #injectstar = star.copy()
     
     data = []
-    for s in noinjectstar:   
+    for s in noinjectstar:  
         datum =  eleanor.TargetData(s, height=15, width=15, bkg_size=31, aperture_mode='small', do_psf=False, do_pca=True, regressors='corner')
         q = datum.quality == 0
 
         lc_orig = datum.to_lightkurve()
         lc_orig = lc_orig.normalize() ##MAKE SURE TO NORMALIZE BEFORE INJECTING else you add tiny number to raw flux val
-
+        #print(lc_orig)
         lc_orig= lc_orig.copy() #we need this, it makes sure the flux u.quantity works with my math, otherwise if you do .remove_outliers in next step its fine
-
+        #print(lc_orig)
         #lc_injected.scatter(c='red')
         #lc_orig.scatter()
         ###xtime = lc_injected['time'].copy()
@@ -242,7 +305,7 @@ def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_proce
         ###plt.scatter(lc_orig['time'],lc_orig['flux'])
         ###plt.show()
 
-        lc_orig = lc_orig.remove_outliers(sigma=6).normalize().remove_nans()
+        lc_orig = lc_orig.remove_outliers(sigma=6).remove_nans()#.normalize().remove_nans()
 
         endtime = max(lc_orig.time.value)-1
         starttime = min(lc_orig.time.value)+1
@@ -281,22 +344,24 @@ def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_proce
 
 
 
-        lc_orig = lc_orig[(lc_orig['flux']<1.20)&(lc_orig['flux']>0.80)].remove_nans()#.bin(time_bin_size=binsizedays)
+        lc_orig = lc_orig[(lc_orig['flux']<1.20)&(lc_orig['flux']>0.70)].remove_nans()#.bin(time_bin_size=binsizedays)
         lc_orig['index'] = np.arange(len(lc_orig))#reindex to avoid issues with removing points
-
+        '''
         #################################################
         # cut below expected signal of a 3.5 R_jup planet
         #################################################
         #lower_lim = 0.5
 
-        lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        #lower_lim = (1-(((3.5/9.73116)/stellar_radius)**2))
+        lower_lim = (1-(((3/9.73116)/stellar_radius)**2))
         for row in lc_orig:
             if row['flux']<lower_lim:
                 idmax = row['index']+2
                 idmin = row['index']-1
                 lc_orig['flux'][idmin:idmax]=np.nan
 
-
+        '''
+        #unbindata.append(lc_orig.remove_nans())
         lc_orig = lc_orig.bin(time_bin_size=binsizedays).remove_nans()
         lc_orig = lc_orig.remove_nans()
 
@@ -305,8 +370,8 @@ def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_proce
 
 
     collect = lk.LightCurveCollection(data)
+    noinject = collect.stitch(lambda x: x.remove_nans()) #no need to normalize a 2nd time
 
-    noinject = collect.stitch()
     noinject.scatter()
     
     def init():
@@ -321,12 +386,14 @@ def vary_paramsmult(func, star, df,stellar_radius=1.325, trials=10000, num_proce
         ##noinject.scatter()
         #print(output_table['depth'])
         #rad_min = 0.02115/stellar_radius #now in R_hoststar
-        rad_min = 0.0616576/stellar_radius #now in R_hoststar for a 0.6 Jupiter radii planet
+        ####rad_min = 0.0616576/stellar_radius #now in R_hoststar for a 0.6 Jupiter radii planet
+        rad_min = 0.14/stellar_radius #now in R_hoststar for a 0.6 Jupiter radii planet
         rad_max = 0.2/stellar_radius #now in R_hoststar for a 2 Jupiter radii planet
 
         depths = np.random.uniform(rad_min, rad_max, trials)  # random transit depths to inject
         midtimes = np.random.uniform(min(noinject.time.value), max(noinject.time.value), trials)  # mid-transit times to inject if you want
-        periods = np.random.uniform(0.6,11,trials) # periods to inject
+        ####periods = np.random.uniform(0.6,10,trials) # periods to inject
+        periods = np.random.uniform(0.6,5,trials) # periods to inject
         
         df['depths'] = depths
         df['midtimes'] = midtimes
